7b8605809a7ff52fdb4115d806b333ee1dc99b60
sstnkvc <sasa.stankovic1996@hotmail.com>
2022-09-13 23:06:08 +0200

    add routing and header styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
+import { PostListComponent } from './posts/posts-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostListComponent },
+  { path: 'create', component: PostsCreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
